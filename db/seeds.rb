7c8ce19e778a285223912f3163c85460955a9cd2
yusuke <>
2021-05-24 22:22:53 +0900

    fakerによるダミーデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Article.create({title: "新しい記事だよ", content: "素晴らしい記事ですね"})
-Article.create({title: "hfhfhfhfhfh", content: "yeahhhhhhhhhhhhhh"})
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
