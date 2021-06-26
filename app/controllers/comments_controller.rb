5af64e4b41f9a78f7a2abc4f3df0a1c0669946f4
yusuke <>
2021-06-26 17:46:07 +0900

    createアクション

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,5 +4,20 @@ class CommentsController < ApplicationController
     article = Article.find(params[:article_id])
     @comment = article.comments.build
   end
-  
+
+  def create
+    article = Article.find(params[:article_id])
+    @comment = article.comments.build(comment_params)
+    if @comment.save
+      redirect_to article_path(article), notice: 'コメント追加'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
