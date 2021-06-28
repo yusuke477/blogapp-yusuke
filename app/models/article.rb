6ddfafc6df9cbe4d4def2cd9c15f3e30cc101c8c
yusuke <>
2021-06-28 14:11:13 +0900

    Likeモデルを作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -26,6 +26,7 @@ class Article < ApplicationRecord
 
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def display_created_at
     I18n.l(self.created_at, format: :default)
